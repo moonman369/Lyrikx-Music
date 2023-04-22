8c5d47ea0f3b81e4f830c86b7ffbde3d36231dfb
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-04-22 10:44:55 +0530

    Minor fix to genre selector element

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1873,15 +1873,13 @@ const Discover = () => {
   if (error) return <Error />;
   return (
     <div className="flex flex-col">
-      <div className="w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">
           Discover {genreTitle}
         </h2>
         <select
-          name=""
-          id=""
           onChange={() => {}}
-          value=""
+          value={genreTitle}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
