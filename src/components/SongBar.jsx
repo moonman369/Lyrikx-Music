09e7502454d6e5226c421fe1d469649b9cbe8ce6
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-04-29 14:18:40 +0530

    Minor change to song and artist details page

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -23,8 +23,8 @@ const SongBar = ({
 
   return (
     <div
-      className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
-        activeSong?.title === song?.title ? "bg-[#4c426e]" : "bg-transparent"
+      className={`w-full flex flex-row items-center hover:bg-[#158b68be] ${
+        activeSong?.title === song?.title ? "bg-[#99999938]" : "bg-transparent"
       } py-2 p-4 rounded-lg cursor-pointer mb-2`}
     >
       <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
