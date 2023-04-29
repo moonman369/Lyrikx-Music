8c9a6e5f9768d1ace9c565a563732a45cf2555a7
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-04-29 14:40:04 +0530

    Minor change to mobile sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = () => {
       </div>
 
       <div
-        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#3d8b87] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-transparent backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
           mobileMenuOpen ? "left-0" : "-left-full"
         }`}
       >
